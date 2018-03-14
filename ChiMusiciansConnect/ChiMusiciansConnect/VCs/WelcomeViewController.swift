d79c07cb2ebeac7c486909121f9735f787bee4f7
Samantha Cannillo <scannillo@Samanthas-MacBook-Pro.local>
2018-03-14 18:09:05 -0500

    D.I.Chi App

---
+++ ChiMusiciansConnect/ChiMusiciansConnect/VCs/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  ChiMusiciansConnect
+//
+//  Created by Samantha Cannillo on 2/27/18.
+//  Copyright © 2018 Samantha Cannillo. All rights reserved.
+//
+
+import UIKit
+
+//This VC class is the first screen upon launch. Houses the login and register buttons.
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) { }
+    
+    @IBAction func unwindToVC1(segue:UIStoryboardSegue) { }
+
+
+}
