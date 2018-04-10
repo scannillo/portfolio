b9394f299335b46068df89f866cfa6c014d164cc
Samantha Cannillo <scannillo@Samanthas-MacBook-Pro.local>
2018-04-09 22:09:26 -0500

    update

--- 3_MySQL_database/SQL_files/procedures_final.sql
+++ 3_MySQL_database/SQL_files/procedures_final.sql
@@ -6,21 +6,21 @@ USE scannilloDB;
 ##table to be the NEW average rating of all previous ratings.
 DELIMITER |
 CREATE PROCEDURE updateMusicianRating(
-	IN musicianNameInput				VARCHAR(75),
-    IN musicianRatingInput				FLOAT
+    IN musicianNameInput                VARCHAR(75),
+    IN musicianRatingInput              FLOAT
 )
 BEGIN
 
-	DECLARE newAvgRating FLOAT;
+    DECLARE newAvgRating FLOAT;
 
-	INSERT INTO LogMusicianRatings
+    INSERT INTO LogMusicianRatings
     VALUES(musicianNameInput, musicianRatingInput, CURRENT_USER(), NOW());
     
     SET newAvgRating = (SELECT AVG(LogRating)
-						FROM LogMusicianRatings L
-						WHERE L.MusicianName = musicianNameInput);
+                        FROM LogMusicianRatings L
+                        WHERE L.MusicianName = musicianNameInput);
                                         
-	UPDATE Musician
+    UPDATE Musician
     SET MusicianRating = newAvgRating
     WHERE MusicianName = musicianNameInput;
     
@@ -30,21 +30,21 @@ DELIMITER ;
 ##Procedure to keep track of booker ratings in the log, but also to update the average in the booker profile table.
 DELIMITER |
 CREATE PROCEDURE updateBookerRating(
-	IN bookerNameInput				VARCHAR(75),
-    IN bookerRatingInput			FLOAT
+    IN bookerNameInput              VARCHAR(75),
+    IN bookerRatingInput            FLOAT
 )
 BEGIN
 
-	DECLARE newAvgRating FLOAT;
+    DECLARE newAvgRating FLOAT;
 
-	INSERT INTO LogBookerRatings
+    INSERT INTO LogBookerRatings
     VALUES(bookerNameInput, bookerRatingInput, CURRENT_USER(), NOW());
     
     SET newAvgRating = (SELECT AVG(LogRating)
-						FROM LogBookerRatings L
-						WHERE L.BookerName = bookerNameInput);
+                        FROM LogBookerRatings L
+                        WHERE L.BookerName = bookerNameInput);
                                         
-	UPDATE Booker
+    UPDATE Booker
     SET BookerRating = newAvgRating
     WHERE BookerName = bookerNameInput;
     
@@ -55,26 +55,26 @@ DELIMITER ;
 ## Venue ratings also are based on genres. If no rating exists for that genre, insert. If so, update to current avg.
 DELIMITER |
 CREATE PROCEDURE updateVenueRating(
-	IN venueNameInput				VARCHAR(75),
-    IN genreCodeInput	        	VARCHAR(5),
-	IN venueRatingInput				FLOAT
+    IN venueNameInput               VARCHAR(75),
+    IN genreCodeInput               VARCHAR(5),
+    IN venueRatingInput             FLOAT
 )
 BEGIN
 
-	DECLARE newAvgRating FLOAT;
+    DECLARE newAvgRating FLOAT;
 
-	INSERT INTO LogVenueRatings
+    INSERT INTO LogVenueRatings
     VALUES(venueNameInput, genreCodeInput, venueRatingInput, CURRENT_USER(), NOW());
     
     SET newAvgRating = (SELECT AVG(LogRating)
-						FROM LogVenueRatings L
-						WHERE L.VenueName = venueNameInput
+                        FROM LogVenueRatings L
+                        WHERE L.VenueName = venueNameInput
                         AND L.GenreCode = genreCodeInput);
                                         
-	INSERT INTO Offers 
+    INSERT INTO Offers 
     VALUES (venueNameInput, genreCodeInput, venueRatingInput)
     ON DUPLICATE KEY UPDATE
-		VenueRating = newAvgRating;
+        VenueRating = newAvgRating;
     
 END |
 DELIMITER ;
